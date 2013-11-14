6b463c7fb2965da740e2d59d7754ae1edf65b07f
张忻正 <zhangxinzheng@huohua.in>
2013-11-14 15:02:01 +0900

    cache the blog part

--- srd/seiyu_miyo/crawler/SeiyuBlogHelper.py
+++ srd/seiyu_miyo/crawler/SeiyuBlogHelper.py
@@ -5,6 +5,7 @@ __author__ = 'zhangxinzheng'
 from BeautifulSoup import *
 import urllib2
 import pymongo
+import time
 class SeiyuBlogHelper(object):
     _instance = ''
     @classmethod
@@ -18,18 +19,23 @@ class SeiyuBlogHelper(object):
         self.db = pymongo.Connection().test
 
     def crawlblog(self, prefix, page):
-        returnList = []
-        for i in xrange(1, 30):
-            if len(returnList) == (page+1) * 10:
-                break
+        cache = self.db.blog.find_one({"blogPrefix": prefix})
+        if cache:
+            createTime = int(cache["createTime"])
+            if int(time.time()) - createTime > 60*60:
+                self.db.blog.remove(cache)
             else:
-                url = prefix + "page-" + str(i) + ".html"
-                html = urllib2.urlopen(url)
-                soup = BeautifulSoup(html)
-                for entry in soup.find(id="sub_main").findAll("div", {"class": "entry"}):
-                    blogName = entry.find("h3", {"class":"title"}).find("a").string
-                    blogUrl = entry.find("h3", {"class":"title"}).find("a").get("href")
-                    timeSmap = entry.find("div", {"class":"entry_head"}).find("span").string
-                    returnList.append({"blogName":blogName, "blogUrl":blogUrl, "timeSmap": timeSmap})
-        print returnList
-        return returnList[-10:]
+                return cache["blogList"][page*10: (page+1)*10]
+        returnList = []
+        for i in xrange(1, 20):
+            url = prefix + "page-" + str(i) + ".html"
+            html = urllib2.urlopen(url)
+            soup = BeautifulSoup(html)
+            for entry in soup.find(id="sub_main").findAll("div", {"class": "entry"}):
+                blogName = entry.find("h3", {"class": "title"}).find("a").string
+                blogUrl = entry.find("h3", {"class": "title"}).find("a").get("href")
+                timeSmap = entry.find("div", {"class": "entry_head"}).find("span").string
+                returnList.append({"blogName": blogName, "blogUrl": blogUrl, "timeSmap": timeSmap})
+
+        self.db.blog.insert({"blogPrefix": prefix, "createTime": str(int(time.time())), "blogList": returnList})
+        return returnList[page*10: (page+1)*10]
